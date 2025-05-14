33e2419b4cb5a962828d4a5cc4a2b199e0321152
Andrey Pchelkin <multigramen@multigramen.ru>
2025-05-14 10:44:27 +0400

    add PHP code

---
+++ blocks/recommend.php
@@ -0,0 +1,37 @@
+<div class="recommend">
+    <div class="recommend_header">
+        <p style="font-weight: 700px; font-size: 36px;">Рекомендуем:</p>
+    </div>
+    <div class="recommended_box">
+        <div class="recommend_product">
+            <img src="/img/Product/dulux_recommended.png" alt="" >
+            <div class="title_name">
+                <p>Краска ВД DULUX 3D White мат BW 9л немороз</p>
+                <div class="price">
+                    <p style="font-weight: 700px; font-size: 36px;">3 699 ₽</p>
+                    <img src="/img/guidance/Cart_Button.svg" alt="" class="buy">
+                </div>
+            </div>
+        </div>
+        <div class="recommend_product">
+            <img src="/img/Product/dulux_recommended.png" alt="">
+            <div class="title_name">
+                <p>Краска ВД DULUX 3D White мат BW 9л немороз</p>
+                <div class="price">
+                    <p style="font-weight: 700px; font-size: 36px;">3 699 ₽</p>
+                    <img src="/img/guidance/Cart_Button.svg" alt="" class="buy">
+                </div>
+            </div>
+        </div>
+        <div class="recommend_product">
+            <img src="/img/Product/dulux_recommended.png" alt="">
+            <div class="title_name">
+                <p>Краска ВД DULUX 3D White мат BW 9л немороз</p>
+                <div class="price">
+                    <p style="font-weight: 700px; font-size: 36px;">3 699 ₽</p>
+                    <img src="/img/guidance/Cart_Button.svg" alt="" class="buy">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Краски Online</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <?php require_once ('components/header.php'); ?>
+        <?php require_once('components/content.php'); ?>
+        <?php require_once('components/footer.php'); ?>
+    </div>
+</body>
+</html>
